43a51e8a8ef2b6da04421061c7a8ed497b533eab
scramble45 <scramble@gmail.com>
2023-02-07 17:05:06 -0600

    additional simplifications

--- backend/src/uninstall.sh
+++ backend/src/uninstall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo systemctl stop wifi-resume.service
-sudo systemctl disable wifi-resume.service
-sudo rm /etc/systemd/system/wifi-resume.service
-
-sudo systemctl stop s3-hibernate.service
-sudo systemctl disable s3-hibernate.service
-sudo rm /etc/systemd/system/s3-hibernate.service
+for service in wifi-resume s3-hibernate; do
+  sudo systemctl stop "${service}.service"
+  sudo systemctl disable "${service}.service"
+  sudo rm "/etc/systemd/system/${service}.service"
+done
