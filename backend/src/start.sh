3ed5b8f8fe7c8a937645014da2ee03a677a0ed4e
Rowan Hamilton <scramble@gmail.com>
2023-02-07 11:35:39 -0600

    Update start.sh

--- backend/src/start.sh
+++ backend/src/start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-iw phy0 wowlan enable magic-packet disconnect
+echo -e "[Unit]\nDescription=Wifi Resume\n\n[Service]\nType=oneshot\nExecStart=/usr/bin/rfkill unblock wifi\n\n[Install]\nWantedBy=suspend.target" | sudo tee /etc/systemd/system/wifi-resume.service > /dev/null && sudo systemctl enable wifi-resume.service
+sudo iw phy0 wowlan enable magic-packet disconnect
+
