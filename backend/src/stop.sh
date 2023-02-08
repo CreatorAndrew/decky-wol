037722da66c3082ad9fc714f7494fe2c51657775
scramble45 <scramble@gmail.com>
2023-02-07 18:53:46 -0600

    cleanup

--- backend/src/stop.sh
+++ backend/src/stop.sh
@@ -2,7 +2,7 @@
 
 for service in wifi-resume s3-hibernate; do
   sudo systemctl stop "${service}.service"
-  sudo systemctl disable "${service}.service"
+  sudo systemctl disable --now "${service}.service" > /dev/null
 done
 
 sudo iw phy0 wowlan disable
--- backend/src/uninstall.sh
+++ backend/src/uninstall.sh
@@ -2,6 +2,7 @@
 
 for service in wifi-resume s3-hibernate; do
   sudo systemctl stop "${service}.service"
-  sudo systemctl disable "${service}.service"
-  sudo rm "/etc/systemd/system/${service}.service"
+  sudo systemctl disable --now "${service}.service" > /dev/null
+  sudo rm -f "/etc/systemd/system/${service}.service"
+  sudo systemctl daemon-reload
 done
