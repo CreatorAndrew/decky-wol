5df5a4dc55e5456890f5f5cd0372d96df8ddc8ca
scramble45 <scramble@gmail.com>
2023-02-08 22:33:20 -0600

    update scripts

--- defaults/bin/start.sh
+++ defaults/bin/start.sh
@@ -2,4 +2,5 @@
 
 echo -e "[Unit]\nDescription=Wifi Resume\n\n[Service]\nType=oneshot\nExecStart=/usr/bin/rfkill unblock wifi\n\n[Install]\nWantedBy=suspend.target" | sudo tee /etc/systemd/system/wifi-resume.service > /dev/null && sudo chmod 644 /etc/systemd/system/wifi-resume.service && sudo systemctl enable wifi-resume.service
 echo -e "[Unit]\nDescription=S3 Hibernate Interrupt\n\n[Service]\nType=oneshot\nExecStart=/usr/bin/systemctl suspend\n\n[Install]\nWantedBy=hibernate.target" | sudo tee /etc/systemd/system/s3-hibernate.service > /dev/null && sudo chmod 644 /etc/systemd/system/s3-hibernate.service && sudo systemctl enable s3-hibernate.service
+sudo systemctl daemon-reload > /dev/null
 sudo iw phy0 wowlan enable magic-packet disconnect
--- defaults/bin/stop.sh
+++ defaults/bin/stop.sh
@@ -3,6 +3,7 @@
 for service in wifi-resume s3-hibernate; do
   sudo systemctl stop "${service}.service"
   sudo systemctl disable --now "${service}.service" > /dev/null
+  sudo systemctl daemon-reload > /dev/null
 done
 
 sudo iw phy0 wowlan disable
