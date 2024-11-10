3394c81e8c5235889fc69d9b50dd5fefeaed7858
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-11-10 10:37:29 -0600

    change formatting

--- defaults/bin/uninstall.sh
+++ defaults/bin/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for service in wifi-resume s3-hibernate; do
-  sudo systemctl stop "${service}.service" > /dev/null 2>&1
-  sudo systemctl disable --now "${service}.service" > /dev/null 2>&1
-  sudo rm -f "/etc/systemd/system/${service}.service" > /dev/null 2>&1
-  sudo systemctl daemon-reload > /dev/null 2>&1
+    sudo systemctl stop "${service}.service" > /dev/null 2>&1
+    sudo systemctl disable --now "${service}.service" > /dev/null 2>&1
+    sudo rm -f "/etc/systemd/system/${service}.service" > /dev/null 2>&1
+    sudo systemctl daemon-reload > /dev/null 2>&1
 done
