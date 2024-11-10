22bc36bb5b4b52fa75689bbbd47b06b6217099ce
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-11-10 09:38:13 -0600

    persist the setting of whether wake-on-LAN is on or off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from os.path import dirname, realpath
+from json import dump, load
+from os.path import basename, dirname, exists, realpath
 from pathlib import Path
 from subprocess import PIPE, Popen, run
 from sys import path
@@ -7,7 +8,18 @@ from sys import path
 path.append(dirname(realpath(__file__)) + "/py_modules")
 
 PLUGIN_DIR = str(Path(__file__).parent.resolve())
+
+
+def get_folder_for_plugin(folder):
+    return f"{dirname(dirname(PLUGIN_DIR))}/{folder}/{basename(PLUGIN_DIR)}"
+
+
 PLUGIN_BIN_DIR = PLUGIN_DIR + "/bin"
+PLUGIN_STATE_FILE = get_folder_for_plugin("data") + "/state.json"
+
+if not exists(PLUGIN_STATE_FILE):
+    dump({"running": False}, open(PLUGIN_STATE_FILE, "w"))
+state = load(open(PLUGIN_STATE_FILE, "r"))
 
 
 def get_wol_status() -> str:
@@ -34,7 +46,8 @@ def is_running() -> bool:
 
 class Plugin:
     async def _main(self):
-        pass
+        if state["running"]:
+            run("./start.sh", cwd=PLUGIN_BIN_DIR, shell=True)
 
     async def _unload(self):
         await self.uninstall()
@@ -49,6 +62,8 @@ class Plugin:
             run("./stop.sh", cwd=PLUGIN_BIN_DIR, shell=True)
         else:
             run("./start.sh", cwd=PLUGIN_BIN_DIR, shell=True)
+        state["running"] = is_running()
+        dump(state, open(PLUGIN_STATE_FILE, "w"))
         return is_running()
 
     # Stop WOL
